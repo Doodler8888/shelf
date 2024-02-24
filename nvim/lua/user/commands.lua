4d2f1050321b02686e46e48d5d44c61756c852bf
wurfkreuz <wurfkreuz@mail.ru>
2024-02-24 22:30:51 +0300

    n

--- nvim/lua/user/commands.lua
+++ nvim/lua/user/commands.lua
@@ -1,12 +1,3 @@
--- Open netrw in specific directories
--- vim.cmd('command! Dot Oil ~/.dotfiles')
--- vim.cmd('command! Symbols Oil /usr/share/X11/xkb/symbols')
--- vim.cmd('command! Bash Oil ~/.dotfiles/bash')
--- vim.cmd('command! Nvim Oil ~/.dotfiles/nvim')
--- vim.cmd('command! Emacs Oil ~/.emacs.d/')
--- vim.cmd('command! Home Oil ~/')
-
--- Open specific files
 vim.cmd('command! Bsh lcd $HOME/.dotfiles/bash | edit .bashrc')
 vim.cmd('command! Inpt lcd $HOME/.dotfiles/bash | edit .inputrc')
 vim.cmd('command! Nvm lcd $HOME/.dotfiles/nvim | edit init.lua')
@@ -21,7 +12,5 @@ vim.cmd('command! Books lcd $HOME/.secret_dotfiles/ansible/playbooks')
 vim.cmd('command! Keys lcd $HOME/.secret_dotfiles/zsh | edit keys.sh')
 vim.cmd('command! Str lcd $HOME/.dotfiles/starship/ | edit starship.toml')
 vim.cmd('command! Scr lcd $HOME/.dotfiles/scripts | Oil ~/.dotfiles/scripts')
-vim.cmd('command! Raku lcd $HOME/.dotfiles/scripts/raku/ | Oil ')
-
--- :set filetype?
-
+vim.cmd('command! Raku lcd $HOME/.dotfiles/scripts/raku/ | Oil ~/.dotfiles/scripts/raku/')
+vim.cmd('command! Dot lcd $HOME/.dotfiles/ | Oil ~/.dotfiles/')
