3e7f14db57b0245f4ed6d4a7cf0f0c552006ff9c
wurfkreuz <wurfkreuz@mail.ru>
2024-04-04 10:03:20 +0300

    n

--- cmd/main.go
+++ cmd/main.go
@@ -17,7 +17,7 @@ func main() {
 	db.InitDB()
 	var (
 		port      = "8080"
-		publicURL = "https://f07b-178-121-0-35.ngrok-free.app"
+		publicURL = "https://6fbe-178-121-0-35.ngrok-free.app"
 		token     = os.Getenv("SHELF_TOKEN")
 	)
 
